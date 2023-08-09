8f07bb09668cf1cdd40cd168b623396bc286f812
Gyudong-Kim <dong3955@gmail.com>
2023-08-09 14:19:05 +0900

    [Feat] challenge model 생성

---
+++ src/models/challenge.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+class Challenge extends Sequelize.Model {
+    static initiate(sequelize) {
+        Challenge.init(
+            {
+                title: {
+                    type: Sequelize.STRING(100),
+                    allowNull: false,
+                },
+                content: {
+                    type: Sequelize.STRING(1000),
+                    allowNull: false,
+                },
+                image: {
+                    type: Sequelize.STRING(100),
+                    allowNull: false,
+                },
+            },
+            {
+                sequelize,
+                timestamps: true,
+                underscored: true,
+                modelName: "Challenge",
+                tableName: "challenges",
+                paranoid: false,
+                charset: "utf8mb4",
+                collate: "utf8mb4_general_ci",
+            }
+        );
+    }
+
+    static associate(db) {
+        db.Challenge.hasMany(db.ChallengerStatus, {
+            foreignKey: "challengeStep",
+            sourceKey: "id",
+        });
+    }
+}
+
+module.exports = Challenge;
