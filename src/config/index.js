dfe64f3c5fcb9c6273212c1a1914a93a17e8d4cc
Gyudong-Kim <dong3955@gmail.com>
2023-08-13 18:25:55 +0900

    [Fix] Port Number INT형으로 변환

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    port: process.env.PORT,
+    port: parseInt(process.env.PORT),
     database: {
         username: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         host: process.env.DATABASE_HOST,
         dialect: process.env.DATABASE_DIALECT,
+        timezone: process.env.DATABASE_TIMEZONE,
     },
 };
