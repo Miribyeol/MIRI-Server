8e2142169e467b052c5be1526861ea1a17648cbf
Gyudong-Kim <dong3955@gmail.com>
2023-08-14 05:50:58 +0900

    [Feat] user Router 설정

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const auth = require("./auth");
+const user = require("./user");
 const router = Router();
 
 router.use("/auth", auth);
+router.use("/user", user);
 
 module.exports = router;
