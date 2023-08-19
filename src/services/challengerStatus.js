112a56ee2b61e747be87bb45670d13df17c91e4d
Gyudong-Kim <dong3955@gmail.com>
2023-08-19 22:23:35 +0900

    [Feat] 챌린지 단계 갱신 서비스 구현

--- src/services/challengerStatus.js
+++ src/services/challengerStatus.js
@@ -12,4 +12,18 @@ exports.getChallengerStep = async (userId) => {
     }
 };
 
+exports.updateChallengerStep = async (userId, challengeStep) => {
+    try {
+        const challengerStatus = await ChallengerStatus.findOne({
+            userId: userId,
+        });
+        challengerStatus.challengeStep = challengeStep;
+        await challengerStatus.save();
+        return challengerStatus.challengeStep;
+    } catch (err) {
+        console.log(err);
+        return new throwError();
+    }
+};
+
 module.exports = exports;
