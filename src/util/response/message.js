d3c58f946b2ce9bb490bc1e27b3b9b8c7c2b72ee
Gyudong-Kim <dong3955@gmail.com>
2023-08-20 18:34:02 +0900

    [Feat] Response Status, message 정의

---
+++ src/util/response/message.js
@@ -0,0 +1,42 @@
+module.exports = Object.freeze({
+    /* AUTH */
+    LOGIN_SUCCESS: "로그인에 성공했습니다.",
+    LOGIN_FAIL: "로그인에 실패하였습니다.",
+
+    SIGNUP_SUCCESS: "회원가입에 성공했습니다.",
+
+    TOKEN_EXPIRED: "토큰이 만료되었습니다.",
+
+    /* USER */
+    GET_NICKNAME_SUCCESS: "사용자 닉네임을 불러왔습니다.",
+    GET_NICKNAME_FAIL: "사용자 닉네임을 불러오지 못했습니다.",
+
+    PATCH_NICKNAME_SUCCESS: "사용자 닉네임을 변경했습니다.",
+    PATCH_NICKNAME_FAIL: "사용자 닉네임을 변경하지 못했습니다.",
+
+    /* PET */
+    CHECK_PET_EXIST_SUCCESS: "반려동물 등록이 완료된 사용자입니다.",
+    CHECK_PET_EXIST_FAIL: "반려동물 등록이 되지 않은 사용자입니다.",
+
+    GET_PET_INFO_SUCCESS: "사용자의 반려동물 정보를 불러왔습니다.",
+    GET_PET_INFO_FAIL: "사용자의 반려동물 정보를 불러오지 못했습니다.",
+
+    POST_PET_INFO_SUCCESS: "사용자의 반려동물 정보를 등록했습니다.",
+    POST_PET_INFO_FAIL: "사용자의 반려동물 정보를 등록하지 못했습니다.",
+    EXIST_PET: "반려동물 정보가 이미 존재하는 사용자입니다.",
+
+    PUT_PET_INFO_SUCCESS: "사용자의 반려동물 정보를 변경했습니다.",
+    PUT_PET_INFO_FAIL: "사용자의 반려동물 정보를 변경하지 못했습니다.",
+
+    /* MAIN */
+    GET_MAIN_SUCCESS: "메인 페이지를 불러왔습니다.",
+    GET_MAIN_FAIL: "메인 페이지를 불러오지 못했습니다.",
+
+    /* EMOTION */
+    PATCH_EMOTION_SUCCESS: "선택한 감정을 반영했습니다.",
+    PATCH_EMOTION_FAIL: "선택한 감정을 반영하지 못했습니다.",
+
+    /* CHALLENGE */
+    PATCH_CHALLENGE_STATUS_SUCCESS: "챌린지 상태를 갱신했습니다.",
+    PATCH_CHALLENGE_STATUS_FAIL: "챌린지 상태를 갱신하지 못했습니다.",
+});
