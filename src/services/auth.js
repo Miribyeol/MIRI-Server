1d1949571c6ace0a021d87aad259ae7ee0d4ba85
Gyudong-Kim <dong3955@gmail.com>
2023-08-20 18:39:10 +0900

    [Refactor] auth 서비스 리펙토링

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,64 +1,56 @@
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
-const config = require("../config");
 const User = require("../models/user");
 const ChallengerStatus = require("../models/challengerStatus");
+const config = require("../config");
 
 exports.getUserByKakao = async (accessToken) => {
     try {
-        const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
+        const getUserProfile = await axios.get(config.kakaoURL, {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
             },
         });
-
-        const userProfile = result.data;
-        const exUser = await User.findOne({ where: { snsId: userProfile.id } });
-
-        if (exUser) {
-            return [exUser, false];
-        } else {
-            const hasGender = () => {
-                if (userProfile.kakao_account.has_gender) {
-                    return userProfile.kakao_account.gender;
-                } else {
-                    return null;
-                }
-            };
-            const hasAgeRange = () => {
-                if (userProfile.kakao_account.has_age_range) {
-                    return userProfile.kakao_account.age_range;
-                } else {
-                    return null;
-                }
-            };
-
-            const newUser = await User.create({
-                email: userProfile.kakao_account.email,
-                nickname: userProfile.kakao_account.profile.nickname,
-                gender: hasGender(),
-                ageRange: hasAgeRange(),
-                snsId: userProfile.id,
-            });
-            await ChallengerStatus.create({
-                userId: newUser.id,
-                challengeStep: 1,
-            });
-            return [newUser, true];
-        }
+        const userProfile = getUserProfile.data;
+        return userProfile;
     } catch (err) {
         console.log(err);
+        return null;
     }
 };
 
-exports.generateToken = async (user) => {
-    try {
-        const userId = user.id;
-        const token = jwt.sign({ userId }, config.jwt.secret, {
-            expiresIn: config.jwt.expiresIn,
+exports.authOrCreateUser = async (userProfile) => {
+    const exUser = await User.findOne({ where: { snsId: userProfile.id } });
+    if (exUser) {
+        return { userId: exUser, isNewUser: false };
+    } else {
+        const hasGender = userProfile.kakao_account.has_gender
+            ? userProfile.kakao_account.gender
+            : null;
+        const hasAgeRange = userProfile.kakao_account.has_age_range
+            ? userProfile.kakao_account.age_range
+            : null;
+
+        const newUser = await User.create({
+            email: userProfile.kakao_account.email,
+            nickname: userProfile.kakao_account.profile.nickname,
+            gender: hasGender(),
+            ageRange: hasAgeRange(),
+            snsId: userProfile.id,
         });
-        return token;
-    } catch (err) {
-        console.log(err);
+
+        await ChallengerStatus.create({
+            userId: newUser.id,
+            challengeStep: 1,
+        });
+
+        return { userId: newUser.id, isNewUser: true };
     }
+};
+
+exports.generateToken = async (userId) => {
+    const token = jwt.sign({ userId }, config.jwt.secret, {
+        expiresIn: config.jwt.expiresIn,
+    });
+    return token;
 };
