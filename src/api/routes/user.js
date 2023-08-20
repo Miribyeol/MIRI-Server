705e3eed9827c7d52d00505eb31f29765dff37ce
Gyudong-Kim <dong3955@gmail.com>
2023-08-20 18:37:51 +0900

    [Refactor] user 컨트롤러 리펙토링

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -2,30 +2,50 @@ const { Router } = require("express");
 const router = Router();
 const { checkJwt } = require("../middlewares/checkJwt");
 const { getUserNickname, changeUserNickname } = require("../../services/user");
+const {
+    GET_NICKNAME_SUCCESS,
+    GET_NICKNAME_FAIL,
+    PATCH_NICKNAME_SUCCESS,
+    PATCH_NICKNAME_FAIL,
+} = require("../../util/response/message");
+const response = require("../../util/response");
 
-router.get("/nickname", checkJwt, async (req, res) => {
+router.get("/nickname", checkJwt, async (req, res, next) => {
     try {
         const userId = req.userId;
         const nickname = await getUserNickname(userId);
-
-        return res.status(200).json({ nickname: nickname });
+        if (!nickname) {
+            return res
+                .status(400)
+                .json(response.response404(GET_NICKNAME_FAIL));
+        }
+        return res
+            .status(200)
+            .json(response.response200(GET_NICKNAME_SUCCESS, { nickname }));
     } catch (err) {
-        return res.status(500).json({ nickname: "fail" });
+        console.log(err);
+        return next(err);
     }
 });
 
-router.patch("/nickname", checkJwt, async (req, res) => {
+router.patch("/nickname", checkJwt, async (req, res, next) => {
     try {
         const userId = req.userId;
         const nickname = req.body.nickname;
-
-        const result = await changeUserNickname(userId, nickname);
-
-        if (result) {
-            return res.status(200).json({ changeNickname: "success" });
+        const changedNickname = await changeUserNickname(userId, nickname);
+        if (!changedNickname) {
+            return res
+                .status(400)
+                .json(response.response400(PATCH_NICKNAME_FAIL));
         }
+        return res.status(200).json(
+            response.response200(PATCH_NICKNAME_SUCCESS, {
+                nickname: changedNickname,
+            })
+        );
     } catch (err) {
-        return res.status(500).json({ changeNickname: "fail" });
+        console.log(err);
+        return next(err);
     }
 });
 
