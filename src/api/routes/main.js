a49f6cd9a930346833ec7a6f22e51ebd451c997d
Gyudong-Kim <dong3955@gmail.com>
2023-09-05 02:18:05 +0900

    [Fix] 챌린지 단계 값이 유효한지 확인 후 메인 뷰 에러처리를 할 수 있도록 수정

--- src/api/routes/main.js
+++ src/api/routes/main.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 const { checkJwt } = require("../middlewares/checkJwt");
-const { getChallengerStep } = require("../../services/challenge");
+const {
+    getChallengerStatus,
+    checkChallengeStep,
+} = require("../../services/challenge");
 const { getEmotions } = require("../../services/emotion");
 const { getPosts } = require("../../services/post");
 const {
@@ -13,15 +16,16 @@ const response = require("../../util/response");
 router.get("/", checkJwt, async (req, res, next) => {
     try {
         const userId = req.userId;
-        const challengerStep = await getChallengerStep(userId);
+        const challengeStep = await getChallengerStatus(userId);
+        const isChallengeStepValid = checkChallengeStep(challengeStep);
         const emotions = await getEmotions();
         const posts = await getPosts();
-        if (!challengerStep || !emotions || !posts) {
+        if (!isChallengeStepValid || !emotions || !posts) {
             return res.status(400).json(response.response400(GET_MAIN_FAIL));
         }
         return res.status(200).json(
             response.response200(GET_MAIN_SUCCESS, {
-                challengerStep,
+                challengeStep,
                 emotions,
                 posts,
             })
