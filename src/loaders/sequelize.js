b287314e50ef2ffbb7f717e0fa510e645ce200e9
Gyudong-Kim <dong3955@gmail.com>
2023-09-07 02:24:55 +0900

    [Fix] Seeder 동기로 동작하도록 수정

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -3,6 +3,6 @@ const { challengeData, emotionData } = require("./seeder");
 
 module.exports = async () => {
     await db.sequelize.sync({ force: false });
-    challengeData();
-    emotionData();
+    await challengeData();
+    await emotionData();
 };
